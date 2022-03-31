b89a81bd6fdb5ae5f344551a68d370a23b11b985
Nicholas Mullikin <nicholas.mullikin@gmail.com>
2022-03-31 15:41:31 -0400

    cleaning

--- get-btn.sh
+++ get-btn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 DEVICE=$(libinput list-devices | grep "Wacom Intuos BT M Pad" -A1 | awk '/Kernel:/ {print $2}')
-libinput record "$DEVICE" | busybox awk '/EV_KEY \/ BTN_/ { print $12 }' > "$OSCOPE_FIFO"
+libinput record "$DEVICE" | busybox awk '/EV_KEY \/ BTN_/ { print $12 }'
